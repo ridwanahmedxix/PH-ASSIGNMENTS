b1e51bbf0b471c213e8c625ebad3f9dc576943d1
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-07-31 09:22:19 +0600

    p 3 solve

--- final.js
+++ final.js
@@ -27,3 +27,18 @@ function isElevatorSafe(weights) {
     return false;
   }
 }
+
+function calculateAiCost(tokensUsed) {
+  if (typeof tokensUsed !== "number" || tokensUsed < 0) {
+    return "Invalid";
+  }
+  if (tokensUsed <= 500) {
+    return 0;
+  }
+  let extraTokens = tokensUsed - 500;
+  let extraHundreds = Math.floor(extraTokens / 100);
+  let totalCost = extraHundreds * 5;
+  return totalCost;
+}
+
+console.log(calculateAiCost(620));
