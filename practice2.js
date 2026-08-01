b2a1e805593240bad5396f7337f06a3ff8fb3de9
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-01 08:22:43 +0600

    some update

--- practice2.js
+++ practice2.js
@@ -2,14 +2,4 @@ function isElevatorSafe(weights) {
   if (!Array.isArray(weights)) {
     return "Invalid";
   }
-  let totalWeights = 0;
-  for (let i = 0; i < weights.length; i++) {
-    totalWeights = totalWeights + weights[i];
-  }
-
-  if (weights <= 400) {
-    return true;
-  } else {
-    return false;
-  }
 }
