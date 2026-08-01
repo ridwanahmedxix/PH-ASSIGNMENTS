11becbca361d5eb65f5aedb11d85dd58627854fc
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-01 08:28:12 +0600

    Problem 02 Agian Solve

--- practice2.js
+++ practice2.js
@@ -2,4 +2,16 @@ function isElevatorSafe(weights) {
   if (!Array.isArray(weights)) {
     return "Invalid";
   }
+
+  let totalWeights = 0;
+
+  for (let i = 0; i < weights.length; i++) {
+    totalWeights = totalWeights + weights[i];
+  }
+
+  if (totalWeights <= 400) {
+    return true;
+  } else {
+    return false;
+  }
 }
