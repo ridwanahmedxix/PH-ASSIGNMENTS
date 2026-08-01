71d864cff448d8cbb78e9480f678c35d23a5831f
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-01 08:51:25 +0600

    Some Update

--- practice2.js
+++ practice2.js
@@ -1,17 +1,1 @@
-function calculateAiCost(tokenUsed) {
-  if (typeof tokenUsed !== "number" || tokenUsed < 0) {
-    return "Invalid";
-  }
-
-  if (tokenUsed <= 500) {
-    return 0;
-  }
-
-  let extraTokens = tokenUsed - 500;
-  let extraHundreds = Math.floor(extraTokens / 100);
-  let totalCost = extraHundreds * 5;
-
-  return totalCost;
-}
-
-console.log(calculateAiCost(740));
+function calculateAiCost(tokenUsed) {}
--- problem03.js
+++ problem03.js
@@ -12,3 +12,21 @@ function calculateAiCost(tokensUsed) {
 }
 
 console.log(calculateAiCost(620));
+
+function calculateAiCost(tokenUsed) {
+  if (typeof tokenUsed !== "number" || tokenUsed < 0) {
+    return "Invalid";
+  }
+
+  if (tokenUsed <= 500) {
+    return 0;
+  }
+
+  let extraTokens = tokenUsed - 500;
+  let extraHundreds = Math.floor(extraTokens / 100);
+  let totalCost = extraHundreds * 5;
+
+  return totalCost;
+}
+
+console.log(calculateAiCost(740));
