a1374810998c5c8abd1806cb989497868f21f04b
Ridwan Ahmed <ridwancodexr@gmail.com>
2026-08-01 08:55:53 +0600

    Problem 03 solve

--- practice2.js
+++ practice2.js
@@ -1,1 +1,16 @@
-function calculateAiCost(tokenUsed) {}
+function calculateAiCost(tokenUsed) {
+  if (typeof tokenUsed !== "number" || tokenUsed < 0) {
+    return "Invalid";
+  }
+
+  if (tokenUsed <= 500) {
+    return 0;
+  }
+
+  let extraTokens = tokenUsed - 500;
+  let extraHundres = Math.floor(extraTokens / 100);
+  let totalCost = extraHundres * 5;
+  return totalCost;
+}
+
+console.log(calculateAiCost(700));
